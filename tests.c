7d0949d65d0b2c8f9b67a485a6c0a40dae13a94c
kenshi0771 <kenshi0771@gmail.com>
2025-01-26 15:11:49 +0500

    Fix tests in 64bit OS

--- tests.c
+++ tests.c
@@ -1,107 +1,198 @@
 #include "allocator2.h"
 #include <stdio.h>
 
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+
 #define HEAP_SIZE 1000
 uint8_t pool[HEAP_SIZE];
 
+enum TestCase
+{
+    CASE1 = 1,
+    CASE2,
+    CASE3,
+    CASE4,
+    CASE5,
+    CASE6,
+    CASE7,
+    CASE8,
+    CASE9,
+    CASE10,
+    CASE11,
+    CASE12,
+    CASE_MAX,
+};
+
 int main()
 {
-    initHeap((uint8_t *)pool, HEAP_SIZE); // HEAP_SIZE - HEADER
+    initHeap((uint8_t *)pool, HEAP_SIZE);
 
     size_t waitingFreeMem = 0;
-    //CASE1:
-    waitingFreeMem = HEAP_SIZE - HEADER_SIZE;
-    if (getFreeMem() != waitingFreeMem)
-        printf("FAILED: CASE1\n");
-    else
-        printf("SUCCEED: CASE1\n");
+    uint8_t* buf1;
+    uint8_t* buf2 = NULL;
+    uint8_t* buf3 = NULL;
 
-    //CASE2:
-    waitingFreeMem -= 100 + HEADER_SIZE;
-    uint8_t *buf = (uint8_t *)ma(100);
-    if (!buf || (getFreeMem() != waitingFreeMem))
-        printf("FAILED: CASE2\n");
-    else
-        printf("SUCCEED: CASE2\n");
+    const size_t AL_SIZE_1 = 100;
+    const size_t AL_SIZE_2 = 200;
+    const size_t AL_SIZE_3 = 500;
+    const size_t RE_SIZE_1 = 52;
+    const size_t RE_SIZE_2 = 10;
+    const size_t RE_SIZE_3 = 201;
 
-    //CASE3:
-    uint8_t *buf2 = (uint8_t *)ma(200);
-    waitingFreeMem -= 200 + HEADER_SIZE;
-    if (!buf2 || (getFreeMem() != waitingFreeMem))
-        printf("FAILED: CASE3\n");
-    else
-        printf("SUCCEED: CASE3\n");
+    for(int currentTestCase = CASE1; currentTestCase < CASE_MAX;currentTestCase++)
+    {
+        bool err = false;
+        switch (currentTestCase)
+        {
+            case CASE1:
+            {
+                waitingFreeMem = HEAP_SIZE - HEADER_SIZE;
+                err = getFreeMem() != waitingFreeMem;
+                break;
+            }
 
-    //CASE4:
-    uint8_t *buf3 = (uint8_t *)ma(500);
-    waitingFreeMem -= 500 + HEADER_SIZE;
-    if (!buf3 || (getFreeMem() != waitingFreeMem))
-        printf("FAILED: CASE4\n");
-    else
-        printf("SUCCEED: CASE4\n");
+            case CASE2:
+            {
+                size_t alSize = AL_SIZE_1;
+                if(alSize % ALIGMENT)
+                    alSize += ALIGMENT - alSize % ALIGMENT;
+                waitingFreeMem -= alSize + HEADER_SIZE;
+                buf1 = (uint8_t *)ma(alSize);
+                err = (!buf1 || (getFreeMem() != waitingFreeMem));
+                break;
+            }
 
-    //CASE5:
-    fr(buf2); //free + 200 cause in middle
-    waitingFreeMem += 200;
-    if (getFreeMem() != waitingFreeMem)
-        printf("FAILED: CASE5\n");
-    else
-        printf("SUCCEED: CASE5\n");
+            case CASE3:
+            {
+                size_t alSize = AL_SIZE_2;
+                buf2 = (uint8_t *)ma(alSize);
+                waitingFreeMem -= alSize + HEADER_SIZE;
+                if(alSize % ALIGMENT)
+                    waitingFreeMem -= ALIGMENT - alSize % ALIGMENT;
 
-    //CASE6:
-    buf2 = (uint8_t *)ma(10);
-    waitingFreeMem -= 12 + HEADER_SIZE; // cause aligment
-    if (!buf2 || (getFreeMem() != waitingFreeMem))
-        printf("FAILED: CASE6\n");
-    else
-        printf("SUCCEED: CASE6\n");
+                err = (!buf2 || (getFreeMem() != waitingFreeMem));
+                break;
+            }
 
-    //CASE7:
-    buf3 = (uint8_t *)re(buf3, 201);
-    waitingFreeMem = waitingFreeMem + 500 - 204;
-    if (!buf3 || (getFreeMem() != waitingFreeMem))
-        printf("FAILED: CASE7\n");
-    else
-        printf("SUCCEED: CASE7\n");
+            case CASE4:
+            {
+                size_t alSize = AL_SIZE_3;
+                buf3 = (uint8_t *)ma(alSize);
+                if(alSize % ALIGMENT)
+                    alSize += ALIGMENT - alSize % ALIGMENT;
+                waitingFreeMem -= alSize + HEADER_SIZE;
+                err = (!buf3 || (getFreeMem() != waitingFreeMem));
+                break;
+            }
 
-    //CASE8:
-    buf = (uint8_t *)re(buf, 100);
-    if (!buf || (getFreeMem() != waitingFreeMem))
-        printf("FAILED: CASE8\n");
-    else
-        printf("SUCCEED: CASE8\n");
+            case CASE5:
+            {
+                fr(buf2);
+                waitingFreeMem += AL_SIZE_2;
+                err = (getFreeMem() != waitingFreeMem);
+                break;
+            }
 
-    //CASE9:
-    buf = (uint8_t *)re(buf, 52);
-    waitingFreeMem = waitingFreeMem + 100 - 52 - HEADER_SIZE;
-    if (!buf || (getFreeMem() != waitingFreeMem))
-        printf("FAILED: CASE9\n");
-    else
-        printf("SUCCEED: CASE9\n");
+            case CASE6:
+            {
+                size_t alSize = 10;
+                buf2 = (uint8_t *)ma(alSize);
+                if(alSize % ALIGMENT)
+                    alSize += ALIGMENT - alSize % ALIGMENT;
+                waitingFreeMem -= alSize + HEADER_SIZE; 
+                err = (!buf2 || (getFreeMem() != waitingFreeMem));
+                break;
+            }
 
-    //CASE10:
-    fr(buf3);
-    waitingFreeMem += 204 + 2 * HEADER_SIZE; // cause in middle
-    if (getFreeMem() != waitingFreeMem)
-        printf("FAILED: CASE10\n");
-    else
-        printf("SUCCEED: CASE10\n");
+            case CASE7:
+            {
+                size_t reSize = RE_SIZE_3;
+                buf3 = (uint8_t *)re(buf3, reSize);
+                size_t realFrSize = AL_SIZE_3;
+                if(realFrSize % ALIGMENT)
+                    realFrSize += ALIGMENT - realFrSize % ALIGMENT;
 
-    //CASE11:
-    fr(buf);
-    waitingFreeMem += 52 + HEADER_SIZE; // cause next is used
-    if (getFreeMem() != waitingFreeMem)
-        printf("FAILED: CASE11\n");
-    else
-        printf("SUCCEED: CASE11\n");
+                if(reSize % ALIGMENT)
+                    reSize += ALIGMENT - reSize % ALIGMENT;
+                waitingFreeMem = waitingFreeMem + realFrSize - reSize;
+                
+                err = (!buf3 || (getFreeMem() != waitingFreeMem));
+                break;
+            }
 
-    //CASE12:
-    fr(buf2);
-    waitingFreeMem += 12 + 2 * HEADER_SIZE; // cause in middle and aligment
-    if (getFreeMem() != waitingFreeMem)
-        printf("FAILED: CASE12\n");
-    else
-        printf("SUCCEED: CASE12\n");
+            case CASE8:
+            {
+                size_t reSize = 100;
+                buf1 = (uint8_t *)re(buf1, reSize);
+              
+                err = (!buf1 || (getFreeMem() != waitingFreeMem));
+                break;
+            }
+
+            case CASE9:
+            {
+                size_t reSize = RE_SIZE_1;
+                buf1 = (uint8_t *)re(buf1, reSize);
+                size_t realFrSize = AL_SIZE_1;
+                if(realFrSize % ALIGMENT)
+                    realFrSize += ALIGMENT - realFrSize % ALIGMENT;
+
+                if(reSize % ALIGMENT)
+                    reSize += ALIGMENT - reSize % ALIGMENT;
+                waitingFreeMem = waitingFreeMem + realFrSize - reSize - HEADER_SIZE;
+                
+                err = (!buf1 || (getFreeMem() != waitingFreeMem));
+                break;
+            }
+
+            case CASE10:
+            {
+                size_t realFrSize = RE_SIZE_3;
+                if(realFrSize % ALIGMENT)
+                    realFrSize += ALIGMENT - realFrSize % ALIGMENT;
+                waitingFreeMem += realFrSize + 2*HEADER_SIZE;
+                fr(buf3);
+                
+                err =  getFreeMem() != waitingFreeMem;
+                break;
+            }
+
+            case CASE11:
+            {
+                size_t realFrSize = RE_SIZE_1;
+                if(realFrSize % ALIGMENT)
+                    realFrSize += ALIGMENT - realFrSize % ALIGMENT;
+                waitingFreeMem += realFrSize + HEADER_SIZE;
+                fr(buf1);
+
+                err =  getFreeMem() != waitingFreeMem;
+                break;
+            }
+
+            case CASE12:
+            {
+                size_t realFrSize = RE_SIZE_2;
+                if(realFrSize % ALIGMENT)
+                    realFrSize += ALIGMENT - realFrSize % ALIGMENT;
+                waitingFreeMem += realFrSize + 2*HEADER_SIZE;
+                fr(buf2);
+              
+                err =  getFreeMem() != waitingFreeMem;
+                break;
+            }
+
+            
+            default: return 0;
+        }
+
+
+        if(err)
+            printf("%sFAILED: CASE%d%s\n",KRED , currentTestCase, KNRM);
+        else
+            printf("%sSUCCEED: CASE%d%s\n",KGRN, currentTestCase, KNRM);
+    }
 
     return 0;
 }
